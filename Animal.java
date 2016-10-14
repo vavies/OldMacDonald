31791f7d2c2cdbc74e8f6a5f9627634495b70921
vavies <olivia38912@gmail.com>
2016-10-14 14:34:46 -0700

    this is so hard

--- Animal.java
+++ Animal.java
@@ -1,4 +1,28 @@
+public void setup(){
+	Cow c = new Cow("cow", "moo");
+	System.out.println(c.getType()+ " goes "+ c.getSound());
+}
 interface Animal 
 {    
-	   //your code here
+	public String getSound();
+	public String getType();
 }  
+class Cow 
+{     
+     private String myType;
+     private String mySound;
+     public Cow(String type, String sound){
+     	myType = type;
+     	mySound = sound;
+     }
+     public Cow(){
+     	myType = "unknown";
+     	mySound = "unknown";
+     }
+     public String getSound(){
+     	return mySound;
+     }
+     public String getType(){
+     	return myType;
+     }
+}
